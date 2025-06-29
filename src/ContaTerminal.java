94966f7722114bd9c57c08b5cefc604046c32574
nimblefx <lucasgabriel.fla@hotmail.com>
2025-06-29 19:00:36 -0300

    https://github.com/LucasGabrielLadeira/conta-bancaria-java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Por favor, digite o número da agência:");
+        Integer agencia = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Por favor, digite o número da conta:");
+        String conta = scanner.nextLine();
+        System.out.print("Por favor, digite seu nome:");
+        String nomeCliente = scanner.nextLine();
+        System.out.print("Por favor, digite o seu saldo:");
+        double saldo = scanner.nextDouble();
+        scanner.nextLine();
+        scanner.close();
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %s e seu saldo %.2f já está disponível para saque",
+                nomeCliente, agencia, conta, saldo);
     }
 }
